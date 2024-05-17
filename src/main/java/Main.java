5d6c6f36668c0c28b865d16affcdf94ccc47d6d4
javier-a21 <janduezsan4@educacion.navarra.es>
2024-05-17 11:57:31 +0200

    Dependencias añadidas a pom y actualizadas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,28 @@
-//import com.masanz.da.spc.controller.NotaController;
-//import freemarker.template.Configuration;
-//import org.apache.logging.log4j.LogManager;
-//import org.apache.logging.log4j.Logger;
-//import spark.template.freemarker.FreeMarkerEngine;
-//
-//import static spark.Spark.*;
-//
-//public class Main {
-//
-//    private static final Logger logger = LogManager.getLogger(Main.class);
-//
-//    public static void main(String[] args) {
-//
-//        logger.info("ARRANCANDO APLICACION");
-//
-//        staticFileLocation("/public");
-////        port(8080);
-//
-//        FreeMarkerEngine freeMarker = new FreeMarkerEngine();
-//        Configuration configuration = new Configuration(Configuration.VERSION_2_3_0);
-//        configuration.setClassForTemplateLoading(Main.class, "/templates");
-//        freeMarker.setConfiguration(configuration);
-//
-//        get("/", NotaController::servirIndice, freeMarker);
+import com.masanz.da.spc.controller.ReservaController;
+import freemarker.template.Configuration;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import static spark.Spark.*;
+
+public class Main {
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+        logger.info("ARRANCANDO APLICACION");
+
+        staticFileLocation("/public");
+//        port(8080);
+
+        FreeMarkerEngine freeMarker = new FreeMarkerEngine();
+        Configuration configuration = new Configuration(Configuration.VERSION_2_3_0);
+        configuration.setClassForTemplateLoading(Main.class, "/templates");
+        freeMarker.setConfiguration(configuration);
+
+//        get("/", ReservaController::servirIndice, freeMarker);
 //        get("/lista-notas", NotaController::servirLista, freeMarker);
 //        get("/nota/:id", NotaController::servirNota, freeMarker);
 //        get("/crea-nota", NotaController::servirCrearNota, freeMarker);
@@ -35,6 +35,6 @@
 //
 //        init();
 //
-//    }
+    }
 //
-//}
+}
