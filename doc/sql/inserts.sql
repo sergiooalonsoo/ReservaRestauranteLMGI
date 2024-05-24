b4c8da76367168c43b926d4bae56e32ba6bfad4c
sergiooalonsoo <salonsogon@educacion.navarra.es>
2024-05-24 20:41:55 +0200

    Correcion en las insercion.

--- doc/sql/inserts.sql
+++ doc/sql/inserts.sql
@@ -1,9 +1,9 @@
 INSERT INTO reservas_db.reserva  (nombre, turno, numComensales, fecha, numeroTelefono)
-VALUES ("javier","CENA",4,"21-'03-2024",452352);
+VALUES ("Javier","CENA",4,"2024-03-27",123456789);
 INSERT INTO reservas_db.reserva  (nombre, turno, numComensales, fecha, numeroTelefono)
-VALUES ("Sergio","COMIDA",6,"24-'03-2024",425492);
+VALUES ("Sergio","COMIDA",6,"2024-03-27",987654321);
 INSERT INTO reservas_db.reserva  (nombre, turno, numComensales, fecha, numeroTelefono)
-VALUES ("Simon","CENA",2,"21-'03-2024",487252);
+VALUES ("Simon","CENA",2,"2024-03-28",123456789);
 INSERT INTO reservas_db.reserva  (nombre, turno, numComensales, fecha, numeroTelefono)
-VALUES ("Robert","COMIDA",13,"21-'03-2024",78252);
+VALUES ("Robert","COMIDA",13,"2024-03-28",987654321);
 
